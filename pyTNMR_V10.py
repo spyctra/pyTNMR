78853f87443eec559c49e501add428538daa2ad3
Michael W. Malone <57849959+spyctra@users.noreply.github.com>
2025-10-23 16:49:56 -0600

    tested

--- pyTNMR_V10.py
+++ pyTNMR_V10.py
@@ -7,14 +7,12 @@ This file is tracked as self.current
 pyTNMR was designed in this manner to prevent having large numbers
 of open files which can create issues over large runs.
 
-No error controls are implemented since no errors
-have ever been personally observed.
-
 Michael Malone mwmalone@gmail.com
 """
 
 """CHANGE LOG
 
+2025-10-22 actual debugging
 2025-09-18 The great _ reformat
 2025-09-13 Better logging and folder creation
 2022-07-22 setTable and getTable added - ARA
@@ -50,49 +48,23 @@ class TNMR(object):
                 sequences are actually run. Useful for
                 debugging sequences
         """
+        self.unique = unique
+        self.running = running
 
-        self.root = os.getcwd() + '\\'
-        self.path = self.getPath(path, unique)
-        self.source = self.copy_source()
+        self.root = os.getcwd() + '\\' 
+        self.exp_path = self.get_exp_path(path.replace('/', '\\'))
+        suffix = self.copy_source()
 
         self.tnmr = win32com.client.Dispatch("NTNMR.Application")
         #print(dir(self.tnmr))
         self.current = self.tnmr.GetActiveDocPath
-        self.log_file = open(f'{path}log.txt', 'w')
-        self.log_file.close()
-        self.logger(f'Saving log in {path}log.txt')
-        self.logger(f'Initializing TNMR with {self.current}')
+        
+        self.pyTNMR_log_file = self.init_log('pyTNMR', suffix)
+        self.exp_log_file = self.init_log('exp', suffix)
 
-        self.data_log = open(f'{path}data_log.txt', 'w')
-        self.data_log.close()
-
-        self.running = running
-
-        self.print_self()
+        self.report()
 
         input('Ready? ')
-
-
-    def copy_source(self):
-        source = sys.argv[0]
-
-        print(f'{source = }')
-
-        filename = ntpath.basename(source)
-
-        print(f'{filename = }')
-
-        file_path = path + filename
-
-        c = 0
-
-        while os.path.exists(file_path):
-            c += 1
-            file_path = f'{path}{filename[:-3]}-v{c}.py'
-
-        print(f'{file_path = }')
-
-        shutil.copyfile(source, file_path)
 
 
     def close(self, filename):
@@ -105,35 +77,109 @@ class TNMR(object):
 
         filename = self.name_check(filename)
 
-        self.logger(f'  CLOSED {filename}')
+        self.pyTNMR_log(f'  CLOSED {filename}')
         self.tnmr.CloseFile(filename)
 
 
-    def get_path(self, path, unique):
+    def copy_source(self):
+        source = sys.argv[0]
+
+        print(f'{source = }')
+
+        source_name = ntpath.basename(source)
+
+        print(f'{source_name = }')
+
+        souce_destination = self.exp_path + source_name
+
+        c = 0
+
+        while os.path.exists(souce_destination):
+            c += 1
+            souce_destination = f'{self.exp_path}{source_name[:-3]}-v{c}.py'
+
+        print(f'{souce_destination = }')
+
+        shutil.copyfile(source, souce_destination)
+        
+        if c > 0:
+            suffix = f'-v{c}'
+        else:
+            suffix = ''
+        
+        return suffix
+
+
+    def get_exp_path(self, path):
         if path[-1] != '\\':
             path += '\\'
 
-        path0 = self.root + path + '\\'
+        path0 = path
 
-        if os.path.isdir(path0):
-            print('{path0 = } exists')
+        if self.unique:
+            c = 0
+            while os.path.isdir(path0[:-1] + f'_{c}\\'):
+                c += 1
 
-            if unique:
-                c = 0
-
-                while os.path.isdir(path0):
-                    c += 1
-                    path0 = self.root + path[:-1] + f'_{c}\\'
-
-                os.makedirs(path0)
-
-                return path0
-            else:
-                return path0
+            exp_path = path0[:-1] + f'_{c}\\'          
+            print(f'Making experiment directory {exp_path}')
+            os.makedirs(exp_path)
+            
+            return exp_path
         else:
-            os.makedirs(path0)
+            if os.path.isdir(path0):
+                print(f'{path0 = } exists')
+            else:
+                print(f'Making experiment directory {path0}')
+                
+                os.makedirs(path0)
+            
+            return path0
 
-        return path0
+
+    def get_param(self, param):
+        value = self.tnmr.GetNMRParameter(param)
+        self.pyTNMR_log(f'    - {param} has value {value}')
+
+        return value
+
+
+    def get_table(self, table):
+        value = self.tnmr.GetTable(table)
+        self.pyTNMR_log(f'    - {table} has values {value}')
+
+        return value
+
+    
+    def init_log(self, log_name, suffix):
+        log_file = f'{self.exp_path}{log_name}_log{suffix}.txt'
+        
+        with open(f'{log_file}', 'w') as a:
+            a.write('')
+    
+        return log_file
+
+
+
+    def log(self, line):
+        """
+        selectively prints args to experiment_log.txt
+        """
+
+        with open(f'{self.exp_log_file}', 'a') as a:
+            print(line)
+            a.write(line + '\n')
+
+
+    def name_check(self, filename):
+        if filename[-4:] != '.tnt':
+            filename += '.tnt'
+        if filename.find('/')<0 and filename.find('\\')<0:
+            filename = self.root + self.exp_path + filename
+
+        filename = filename.replace('/','\\')
+        
+        return filename
 
 
     def open(self, filename):
@@ -141,14 +187,14 @@ class TNMR(object):
         Opens a new file and closes the current file
 
         Exits program if file not found
-        will search for filename in self.path if more detailed path not specified
+        will search for filename in self.exp_path if more detailed path not specified
         in filename
         """
 
         filename =self.name_check(filename)
-        self.logger('\n---------------')
-        self.logger(filename[filename.rfind('\\')+1:])
-        self.logger(f' OPENING {filename}')
+        self.pyTNMR_log('\n---------------')
+        self.pyTNMR_log(filename[filename.rfind('\\')+1:])
+        self.pyTNMR_log(f' OPENING {filename}')
 
         #Check if file is already open--happens when using loops
         if filename != self.tnmr.GetActiveDocPath:
@@ -157,44 +203,71 @@ class TNMR(object):
             self.current = self.tnmr.GetActiveDocPath
 
             if filename != self.tnmr.GetActiveDocPath:
-                self.logger('    Could not open')
-                self.logger(filename)
-                self.logger(self.tnmr.GetActiveDocPath)
+                self.pyTNMR_log('    Could not open')
+                self.pyTNMR_log(filename)
+                self.pyTNMR_log(self.tnmr.GetActiveDocPath)
                 input('Exit')
         else:
-            self.logger('  Already Opened')
+            self.pyTNMR_log('  Already Opened')
 
-        self.logger()
+        self.pyTNMR_log()
 
 
-    def print_self(self):
+    def pyTNMR_log(self, line=''):
+        """
+        prints both to the stdout and the experiment log file in the
+        experimental data folder
+        """
+
+        with open(f'{self.pyTNMR_log_file}', 'a') as a:
+            print(line)
+            a.write(line + '\n')
+
+
+    def read(self, filename):
+        return read_TNT(self.exp_path + filename)
+
+
+    def report(self):
+        print()
         print(f'{self.root = }')
-        print(f'{self.path = }')
+        print(f'{self.exp_path = }')
         print(f'{self.running = }')
+        print(f'{self.unique = }')
 
 
-    def get_spyctra(self, *filename):
+    def reset(self):
+        self.pyTNMR_log('Resetting Hardware')
+        self.tnmr.Reset
+
+
+    def restart(self, timeout=3):
         """
-        returns tnt spyctra object
+        Restarts the TNMR program
+        Holdover from old code.
+        Kept for future concerns
         """
+        self.pyTNMR_log("Crashed! Restarting tnmr")
+        os.system("taskkill /F /IM TNMR.exe")
+        self.tnmr = win32com.client.Dispatch("TNMR.Application")
+        sleep(timeout)
+        # for some reason, things are really wonky if self.tnmr is created
+        # without a file loaded. So, load a file and re-create that object.
+        self.open_file(self.sweep_file)
+        self.tnmr = win32com.client.Dispatch("TNMR.Application")
 
-        if filename:
-            return TNT_reader(self.name_check(filename[0]))
-        else:
-            return TNT_reader(self.tnmr.GetActiveDocPath)
 
-
-    def set_table(self, table, value):
-        """Set TNMR table to ###
-
-        args:
-            param (str): the TNMR table to be changed
-            value (str): new tables values, must be comma seperated
-
+    def save_as(self, filename):
         """
-
-        self.logger(f'    -Setting {table} to {value}')
-        self.tnmr.SetTable(table, value)
+        Saves the current file to the specified path
+        """
+        filename = self.name_check(filename)
+        self.pyTNMR_log(f'  SAVING {self.current}')
+        self.pyTNMR_log(f'      AS {filename}')
+        self.tnmr.SaveAs(filename)
+        self.close(self.current)
+        self.current = self.tnmr.GetActiveDocPath
+        self.pyTNMR_log()
 
 
     def set_param(self, param, value):
@@ -206,35 +279,26 @@ class TNMR(object):
 
         """
 
-        self.logger(f'    -Setting {param} to {value}')
+        self.pyTNMR_log(f'    -Setting {param} to {value}')
         self.tnmr.SetNMRParameter(param, value)
 
 
-    def get_param(self, param):
-        value = self.tnmr.GetNMRParameter(param)
-        self.logger(f'    - {param} has value {value}')
+    def set_table(self, table, value):
+        """Set TNMR table to ###
 
-        return value
+        args:
+            param (str): the TNMR table to be changed
+            value (str): new tables values, must be comma seperated
 
-
-    def get_table(self, table):
-        value = self.tnmr.GetTable(table)
-        self.logger(f'    - {table} has values {value}')
-
-        return value
-
-
-    def save_as(self, filename):
         """
-        Saves the current file to the specified path
-        """
-        filename =self.name_check(filename)
-        self.logger(f'  SAVING {self.current}')
-        self.logger(f'      AS {filename}')
-        self.tnmr.SaveAs(filename)
-        self.close(self.current)
-        self.current = self.tnmr.GetActiveDocPath
-        self.logger()
+
+        self.pyTNMR_log(f'    -Setting {table} to {value}')
+        self.tnmr.SetTable(table, value)
+
+
+    def sleep(self, t):
+        if self.running == 1:
+            sleep(t)
 
 
     def zg(self, *manual_check):
@@ -253,11 +317,11 @@ class TNMR(object):
 
         while True:
             if self.running == 1:
-                self.logger('\n RUNNING')
+                self.pyTNMR_log('\n RUNNING')
                 self.tnmr.ZG
                 # Now wait for it to finish.
                 t0 = time()
-                sleep(1)
+                sleep(.1)
                 expected_time_str = self.tnmr.GetNMRParameter("Exp. Elapsed Time")
                 expected_time = [int(val) for val in expected_time_str.split(':')]
                 expected_time = 3600*expected_time[0] + 60*expected_time[1] + expected_time[2]
@@ -265,9 +329,9 @@ class TNMR(object):
                 if expected_time == 0:
                     expected_time = 1
 
-                self.logger(f'{expected_time_str = }')
-                self.logger(f'{expected_time = }')
-                self.logger(f'{total_scans = }')
+                self.pyTNMR_log(f'{expected_time_str = }')
+                self.pyTNMR_log(f'{expected_time = }')
+                self.pyTNMR_log(f'{total_scans = }')
 
                 done = False
                 c = 1
@@ -298,78 +362,6 @@ class TNMR(object):
                 self.zg(manual_check[0])
 
 
-    def read(self, filename):
-        return read_TNT(self.path + filename)
-
-
-    def reset(self):
-        self.logger('Resetting Hardware')
-        self.tnmr.Reset
-
-
-    def restart(self, timeout=3):
-        """
-        Restarts the TNMR program
-        Holdover from old code.
-        Kept for future concerns
-        """
-        self.logger("Crashed! Restarting tnmr")
-        os.system("taskkill /F /IM TNMR.exe")
-        self.tnmr = win32com.client.Dispatch("TNMR.Application")
-        sleep(timeout)
-        # for some reason, things are really wonky if self.tnmr is created
-        # without a file loaded. So, load a file and re-create that object.
-        self.open_file(self.sweep_file)
-        self.tnmr = win32com.client.Dispatch("TNMR.Application")
-
-
-    def sleep(self, t):
-        if self.running == 1:
-            sleep(t)
-
-
-    def log(self, *args):
-        """
-        selectively prints args to data_log.txt
-        """
-
-        self.data_log = open(f'{self.path}data_log.txt', 'a')
-
-        for arg in args:
-            print(arg, end=',', file=self.data_log)
-
-        print(file = self.data_log)
-
-        self.data_log.close()
-
-
-    def logger(self, *args):
-        """
-        prints both to the stdout and the experiment log file in the
-        experimental data folder
-        """
-
-        self.log_file = open(f'{self.path}log.txt', 'a')
-
-        for arg in args:
-            print(arg, end=' ')
-            print(arg, end=' ', file = self.log_file)
-
-        print()
-        print(file = self.log_file)
-
-        self.log_file.close()
-
-
-    def name_check(self, filename):
-        if filename[-4:] != '.tnt':
-            filename += '.tnt'
-        if filename.find('/')<0 and filename.find('\\')<0:
-            filename = self.path + filename
-
-        return filename.replace('/','\\')
-
-
 class TNMRError(Exception):
     """
     A TNMR-specific exception class
@@ -378,18 +370,20 @@ class TNMRError(Exception):
     pass
 
 
+def test_suite():
+    a = TNMR('data', False, 0)
+    a.open(f'{a.root}RO')
+    
+    for rec_gain in [i+60 for i in range(10)]:
+        a.log(f'{rec_gain = }')
+        a.set_param('Receiver Gain', rec_gain)
+        a.zg()
+        a.save_as(f'RO_RecGain{rec_gain}')
+        
+        
 def main():
-    a = TNMR('./', 0)
-    sleep(5)
-    print('a')
-    a.reset()
-    sleep(5)
-    a.reset()
-
+    test_suite()
 
 
 if __name__ == '__main__':
     main()
-
-
-
